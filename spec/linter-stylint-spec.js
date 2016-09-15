bb265189b2aab1c2f175a06c1e30acb8d64b5f70
Landon Abney <Landonabney@gmail.com>
2016-09-15 16:10:13 -0700

    Fix the regex
    Update the regex to account for the completely different output formats encountered when no configuration is provided. Specs added to test this situation so it isn't silently ignored in the future.

--- spec/linter-stylint-spec.js
+++ spec/linter-stylint-spec.js
@@ -2,9 +2,32 @@
 
 import path from 'path';
 
-const badPath = path.join(__dirname, 'fixtures', 'bad.styl');
-const goodPath = path.join(__dirname, 'fixtures', 'good.styl');
-const errorPath = path.join(__dirname, 'fixtures', 'error.styl');
+const multiPath = path.join(__dirname, 'fixtures', 'multi', 'multi.styl');
+const noConfigMultiPath = path.join(__dirname, 'fixtures', 'no-config-multi', 'multi.styl');
+const goodPath = path.join(__dirname, 'fixtures', 'good', 'good.styl');
+const errorPath = path.join(__dirname, 'fixtures', 'error', 'error.styl');
+
+const validateMulti = (messages, filePath) => {
+  expect(messages.length).toBe(3);
+
+  expect(messages[0].type).toBe('warning');
+  expect(messages[0].text).toBe('unnecessary bracket (brackets)');
+  expect(messages[0].html).not.toBeDefined();
+  expect(messages[0].filePath).toBe(filePath);
+  expect(messages[0].range).toEqual([[0, 5], [0, 7]]);
+
+  expect(messages[1].type).toBe('warning');
+  expect(messages[1].text).toBe('missing colon between property and value (colons)');
+  expect(messages[1].html).not.toBeDefined();
+  expect(messages[1].filePath).toBe(filePath);
+  expect(messages[1].range).toEqual([[1, 4], [1, 7]]);
+
+  expect(messages[2].type).toBe('warning');
+  expect(messages[2].text).toBe('unnecessary bracket (brackets)');
+  expect(messages[2].html).not.toBeDefined();
+  expect(messages[2].filePath).toBe(filePath);
+  expect(messages[2].range).toEqual([[2, 0], [2, 1]]);
+};
 
 describe('The stylint provider for Linter', () => {
   const { lint } = require('../lib/init.js').provideLinter();
@@ -30,20 +53,23 @@ describe('The stylint provider for Linter', () => {
     )
   );
 
-  it('finds something wrong with invalid file', () =>
-    waitsForPromise(() =>
-      atom.workspace.open(badPath).then(editor =>
-        lint(editor).then((messages) => {
-          expect(messages.length).toEqual(2);
-          expect(messages[0].type).toBe('warning');
-          expect(messages[0].text).toBe('unnecessary bracket (brackets)');
-          expect(messages[0].html).not.toBeDefined();
-          expect(messages[0].filePath).toBe(badPath);
-          expect(messages[0].range).toEqual([[1, 5], [1, 7]]);
-        })
+  describe('checks a file with multiple issues', () => {
+    it('works with a config', () =>
+      waitsForPromise(() =>
+        atom.workspace.open(multiPath).then(editor =>
+          lint(editor).then(messages => validateMulti(messages, multiPath))
+        )
       )
-    )
-  );
+    );
+
+    it('works without a config', () =>
+      waitsForPromise(() =>
+        atom.workspace.open(noConfigMultiPath).then(editor =>
+          lint(editor).then(messages => validateMulti(messages, noConfigMultiPath))
+        )
+      )
+    );
+  });
 
   it('handles error-level severity', () =>
     waitsForPromise(() =>
@@ -54,7 +80,7 @@ describe('The stylint provider for Linter', () => {
           expect(messages[0].text).toBe('unnecessary colon found (colons)');
           expect(messages[0].html).not.toBeDefined();
           expect(messages[0].filePath).toBe(errorPath);
-          expect(messages[0].range).toEqual([[2, 8], [2, 9]]);
+          expect(messages[0].range).toEqual([[1, 6], [1, 7]]);
         })
       )
     )
